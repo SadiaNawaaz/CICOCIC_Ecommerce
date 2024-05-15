33454051fd741cdef4c2aaff7ec29b4d07db02cd
Sadia Nawaaz <sadianawaaz1995@gmail.com>
2024-05-15 16:32:26 +0500

    Today workings
    Today workings

--- Ecommerce.Web/obj/Debug/net8.0/Ecommerce.Web.AssemblyInfo.cs
+++ Ecommerce.Web/obj/Debug/net8.0/Ecommerce.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a93c9fce8d6409311c5372fb8b9c8655a296a17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
