ff6c1143c95632349df56235b334ebe64a30c7a3
Sadia Nawaaz <sadianawaaz1995@gmail.com>
2024-09-03 14:11:07 +0500

    Role Chanegs
    Role Chanegs

--- Ecommerce.Shared/Entities/Roles/Role.cs
+++ Ecommerce.Shared/Entities/Roles/Role.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Shared.Abstraction;
+using Ecommerce.Shared.Entities.Permissions;
 using System.ComponentModel.DataAnnotations;
 
 namespace Ecommerce.Shared.Entities.Roles;
@@ -8,4 +9,13 @@ public class Role : BaseEntity
     [Required]
     public  string Name { get; set; }
     public string? Description { get; set; }
+    public List<RolePermission> RolePermissions { get; set; }
+}
+
+public class RolePermission : BaseEntity
+{
+    public long RoleId { get; set; }
+    public Role Role { get; set; }
+    public long PermissionId { get; set; }
+    public Permission Permission { get; set; }
 }
